59ae0fcdbe99936a4dbee336cd77117f2cce0a9e
hsimyu <hsimyu1900@gmail.com>
2023-12-18 23:23:02 +0900

    Print member function.

--- src/refl/include/refl.h
+++ src/refl/include/refl.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <cstdio>
+#include <cstddef>
+#include <span>
+#include <variant>
+
 enum class PropertyType
 {
     Int,
@@ -39,3 +44,27 @@ struct PropertyInfo
             &T::##name,                         \
             #name                               \
     }
+
+template <typename T>
+void PrintMembers(const T &obj)
+{
+    const auto properties = std::span{T::TypeInfo::Properties};
+    for (auto &&elem : properties)
+    {
+        switch (elem.pOffset.index())
+        {
+        case 0: // int
+        {
+            auto offset = std::get<0>(elem.pOffset);
+            printf("- %s = %d\n", elem.name, obj.*offset);
+            break;
+        }
+        case 1: // float
+        {
+            auto offset = std::get<1>(elem.pOffset);
+            printf("- %s = %g\n", elem.name, obj.*offset);
+            break;
+        }
+        }
+    }
+}
--- src/refl/main.cpp
+++ src/refl/main.cpp
@@ -3,41 +3,17 @@
 #include <span>
 #include <variant>
 
+#include <refl.h>
 #include <MyClass.h>
 
 int main()
 {
-    constexpr auto view = std::span{MyClass::TypeInfo::Properties};
-
     MyClass c{};
     c.a = 1;
     c.b = 12;
     c.c = 32;
-    MyClass *p = &c;
 
-    printf("BaseAddress = %p\n", p);
-    uintptr_t base = reinterpret_cast<uintptr_t>(p);
-
-    for (auto &&elem : view)
-    {
-        auto memberAddress = base;
-
-        switch (elem.pOffset.index())
-        {
-        case 0: // int
-        {
-            auto offset = std::get<0>(elem.pOffset);
-            printf("- %s = %d\n", elem.name, p->*offset);
-            break;
-        }
-        case 1: // float
-        {
-            auto offset = std::get<1>(elem.pOffset);
-            printf("- %s = %g\n", elem.name, p->*offset);
-            break;
-        }
-        }
-    }
+    PrintMembers(c);
 
     return 0;
 }
